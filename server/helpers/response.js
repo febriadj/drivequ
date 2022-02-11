8863512aa95d63ad1a1d20353ec130294e531c7a
febriadj <iamfebriadji@gmail.com>
2022-02-11 23:37:14 +0700

    :hammer: Add re-usable function to handle router response

---
+++ server/helpers/response.js
@@ -0,0 +1,13 @@
+module.exports = (
+  res,
+  message = null,
+  data = null,
+  httpStatusCode = 200,
+  success = true,
+) => {
+  res.status(httpStatusCode).json({
+    success,
+    message,
+    data,
+  });
+};
