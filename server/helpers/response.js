f91226a650a0cabefc3c20382c45fc6baa42bd76
febriadj <iamfebriadji@gmail.com>
2022-02-13 16:53:23 +0700

    :adhesive_bandage: Fix router response parameter

--- server/helpers/response.js
+++ server/helpers/response.js
@@ -1,10 +1,10 @@
-module.exports = (
+module.exports = ({
   res,
   message = null,
   data = null,
   httpStatusCode = 200,
   success = true,
-) => {
+}) => {
   res.status(httpStatusCode).json({
     success,
     message,
