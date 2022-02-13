e4f7566c42256eaec9bcac75fe10df3299637c84
febriadj <iamfebriadji@gmail.com>
2022-02-13 18:15:35 +0700

    :hammer Update server entry point

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const database = require('./database/connect');
-const routes = require('./routes');
+
+const inRoutes = require('./routes/internal');
 
 database();
 const app = express();
 
-app.use(cors);
-app.use('/api', routes);
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/in', inRoutes);
 
 module.exports = app;
