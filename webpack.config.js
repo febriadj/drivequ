bd10ebf2bf56a1963efe9ed1257b618516f2a654
febriadj <iamfebriadji@gmail.com>
2022-02-15 22:19:14 +0700

    :wrench: Set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client/public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'client/public'),
+    },
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
