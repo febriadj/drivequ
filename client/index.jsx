8349f443ecbf1bad0180ad9b0bf94c3fd0efd377
febriadj <iamfebriadji@gmail.com>
2022-02-18 19:31:37 +0700

    :hammer: Add default base url for http request axios

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import App from './app';
+
+axios.defaults.baseURL = 'http://localhost:5050/api/in';
 
 ReactDOM.render(
   <App />,
