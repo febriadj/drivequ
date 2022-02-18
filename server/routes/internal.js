7a6eaab33582c935fc304f3aa86a4493e63ccd17
febriadj <iamfebriadji@gmail.com>
2022-02-18 19:28:44 +0700

    :hammer: Update server internal routes

--- server/routes/internal.js
+++ server/routes/internal.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const document = require('../controllers/internal/document');
 
 router.post('/documents', document.insert);
+router.get('/documents', document.find);
 
 module.exports = router;
