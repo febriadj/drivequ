8b1053faa20155fbb0b3a06d0ff4bd9624ef8d83
febriadj <iamfebriadji@gmail.com>
2022-02-19 17:57:18 +0700

    :hammer: Add folder database model

---
+++ server/database/models/folder.js
@@ -0,0 +1,37 @@
+const { model, Schema } = require('mongoose');
+
+const FolderModel = model('folders', new Schema({
+  name: {
+    type: Schema.Types.String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  url: {
+    type: Schema.Types.String,
+    unique: true,
+  },
+  permission: {
+    type: Schema.Types.String,
+    enum: ['public', 'private'],
+    default: 'public',
+  },
+  location: {
+    type: Schema.Types.Array,
+    required: true,
+    default: ['/'],
+  },
+  path: {
+    type: Schema.Types.Array,
+    required: true,
+    default: ['/'],
+  },
+  description: {
+    type: Schema.Types.String,
+    default: '',
+  },
+}, {
+  timestamps: true,
+}));
+
+module.exports = FolderModel;
