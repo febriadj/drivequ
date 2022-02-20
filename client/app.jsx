f12cd0ba6c885a1cf208689a44f57def90d50808
febriadj <iamfebriadji@gmail.com>
2022-02-20 18:06:47 +0700

    :hammer: Add router for docInFolder page

--- client/app.jsx
+++ client/app.jsx
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<page.dashboard />} />
+        <Route exact path="/folder/:url" element={<page.docInFolder />} />
       </Routes>
     </BrowserRouter>
   );
