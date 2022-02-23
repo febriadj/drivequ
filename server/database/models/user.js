2490ae0b3c4c7d3a06a1ebce305f07be786fb80f
febriadj <iamfebriadji@gmail.com>
2022-02-23 19:36:32 +0700

    :hammer: Add user database model

---
+++ server/database/models/user.js
@@ -0,0 +1,34 @@
+const { model, Schema } = require('mongoose');
+
+const UserModel = model('users', new Schema({
+  email: {
+    type: Schema.Types.String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  name: {
+    first: {
+      type: Schema.Types.String,
+      trim: true,
+      required: true,
+    },
+    last: {
+      type: Schema.Types.String,
+      trim: true,
+      required: true,
+    },
+  },
+  avatar: {
+    type: Schema.Types.String,
+    default: 'avatar.png',
+  },
+  password: {
+    type: Schema.Types.String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+}));
+
+module.exports = UserModel;
