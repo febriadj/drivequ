ab4295e916dee63f653950b0387bef4e31e605e7
febriadj <iamfebriadji@gmail.com>
2022-02-24 13:56:14 +0700

    :hammer: Update dashboard/table component

--- client/components/dashboard/table.jsx
+++ client/components/dashboard/table.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as icon from 'react-icons/bi';
-import formatDate from '../../helpers/formatDate';
+import * as helper from '../../helpers';
 
-function Table({ documents, folders }) {
+function Table({
+  documents,
+  folders,
+  setSelected,
+}) {
   const navigate = useNavigate();
 
   return (
-    <table className="container border-collapse">
+    <table className="w-full border-collapse">
       <thead>
-        <tr className="py-2.5 grid grid-cols-table-docs">
+        <tr className="py-2.5 grid grid-cols-4/1fr-0.5fr-0.5fr-0.5fr border-0 border-b border-solid border-gray-300">
           <td>Name</td>
           <td>Mimetype</td>
           <td>Published</td>
@@ -21,29 +25,50 @@ function Table({ documents, folders }) {
           folders.map((item) => (
             <tr
               key={item._id}
-              className="py-2.5 grid grid-cols-table-docs items-center border-0 border-b border-solid border-gray-300 cursor-default"
-              onClick={() => navigate(`/folder${item.url}`)}
+              className="py-2.5 grid grid-cols-4/1fr-0.5fr-0.5fr-0.5fr items-center border-0 border-b border-solid border-gray-300 cursor-default"
+              onDoubleClick={() => navigate(`/folder${item.url}`)}
+              onClick={(event) => {
+                const e = event.currentTarget;
+                setSelected([item._id]);
+
+                const tr = [...document.getElementsByTagName('tr')];
+                tr.map((item1) => item1.classList.remove('bg-gray-100'));
+
+                e.classList.add('bg-gray-100');
+              }}
             >
               <td className="flex items-center gap-3.5">
                 <icon.BiFolder className="text-2xl" />
                 <p className="text-base">{item.name}</p>
               </td>
               <td><span className="block w-3 h-px bg-black"></span></td>
-              <td>{formatDate(item.createdAt)}</td>
+              <td>{helper.formatDate(item.createdAt)}</td>
               <td><span className="block w-3 h-px bg-black"></span></td>
             </tr>
           ))
         }
         {
           documents.map((item) => (
-            <tr key={item._id} className="py-2.5 grid grid-cols-table-docs border-0 border-b border-solid border-gray-300 cursor-default">
-              <td className="flex items-center gap-3.5">
-                <icon.BiFile className="text-2xl" />
-                <p className="text-base">{`${item.filename}.${item.format}`}</p>
+            <tr
+              key={item._id}
+              className="py-2.5 grid grid-cols-4/1fr-0.5fr-0.5fr-0.5fr border-0 border-b border-solid border-gray-300 cursor-default"
+              onClick={(event) => {
+                const e = event.currentTarget;
+                setSelected([item._id]);
+
+                const tr = [...document.getElementsByTagName('tr')];
+                tr.map((item1) => item1.classList.remove('bg-gray-100'));
+
+                e.classList.add('bg-gray-100');
+              }}
+            >
+              <td className="grid grid-cols-2/auto-1fr items-center gap-3.5 overflow-x-hidden">
+                <icon.BiFileBlank className="text-2xl" />
+                <p className="truncate">{`${item.filename}.${item.format}`}</p>
               </td>
-              <td>{item.mimetype}</td>
-              <td>{formatDate(item.createdAt)}</td>
-              <td>{`${item.size} KB`}</td>
+              <td className="truncate">{item.mimetype}</td>
+              <td className="truncate">{helper.formatDate(item.createdAt)}</td>
+              <td className="truncate">{helper.bytesToSize(item.size)}</td>
             </tr>
           ))
         }
