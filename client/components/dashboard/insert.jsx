d7290dcf64b64ab2bbe6ad1b818313c44097b1fa
febriadj <iamfebriadji@gmail.com>
2022-02-24 13:55:47 +0700

    :hammer: Update dashboard/insert component

--- client/components/dashboard/insert.jsx
+++ client/components/dashboard/insert.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import * as icon from 'react-icons/bi';
 
-function Insert({ setModal }) {
+function Insert({ setModal, location }) {
+  const handleUploadFile = async (event) => {
+    try {
+      const fd = new FormData();
+      fd.append('file', event.target.files[0]);
+      fd.append('location', location);
+
+      const url = 'http://localhost:5050/api/in/documents';
+      const request = await (await fetch(url, {
+        method: 'post',
+        body: fd,
+      })).json();
+
+      if (!request.success) throw request;
+      console.log(request.payload);
+    }
+    catch (error0) {
+      console.error(error0.message);
+    }
+  };
+
   return (
-    <div className="absolute top-0 w-60 left-0 bg-white shadow-lg shadow-gray-300 translate-y-10">
+    <div className="absolute top-0 w-60 left-0 bg-white shadow-lg shadow-gray-300 translate-y-14">
       <div className="container py-3.5 grid">
         <button
           type="button"
@@ -19,20 +39,39 @@ function Insert({ setModal }) {
           <icon.BiFolderPlus className="text-2xl" />
           <p className="text-base">New Folder</p>
         </button>
-        <button
-          type="button"
-          className="flex items-center gap-5 py-2 px-3.5 hover:bg-gray-100"
-        >
-          <icon.BiFolder className="text-2xl" />
-          <p className="text-base">Upload File</p>
-        </button>
-        <button
-          type="button"
-          className="flex items-center gap-5 py-2 px-3.5 hover:bg-gray-100"
-        >
-          <icon.BiFile className="text-2xl" />
-          <p className="text-base">Upload Folder</p>
-        </button>
+        <span className="block w-full h-px bg-gray-300 my-2.5"></span>
+        <form method="post" encType="multipart/form-data">
+          <label
+            htmlFor="file"
+            className="flex items-center gap-5 py-2 px-3.5 hover:bg-gray-100"
+          >
+            <icon.BiUpload className="text-2xl" />
+            <p className="text-base">Upload File</p>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              onChange={handleUploadFile}
+              className="hidden"
+            />
+          </label>
+          <label
+            htmlFor="folder"
+            className="flex items-center gap-5 py-2 px-3.5 hover:bg-gray-100"
+          >
+            <icon.BiFolder className="text-2xl" />
+            <p className="text-base">Upload Folder</p>
+            <input
+              type="file"
+              name="folder"
+              id="folder"
+              webkitdirectory="true"
+              directory="true"
+              className="hidden"
+              onChange={(event) => console.log(event)}
+            />
+          </label>
+        </form>
       </div>
     </div>
   );
