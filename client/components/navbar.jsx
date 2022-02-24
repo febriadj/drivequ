130de286127f08685b914e44f6003b3d33a0390c
febriadj <iamfebriadji@gmail.com>
2022-02-24 13:54:55 +0700

    :hammer: Update navbar component

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,11 +3,11 @@ import * as icon from 'react-icons/bi';
 
 function Navbar() {
   return (
-    <div className="fixed w-full h-16 border border-gray-300 border-solid border-x-0 border-t-0 bg-white z-50">
-      <div className="w-full py-2 px-5 grid grid-cols-navbar items-center">
-        <div className="flex items-center gap-2.5 w-60">
-          <icon.BiPackage className="text-4xl" />
-          <h3 className="text-xl">Storage</h3>
+    <div className="absolute w-full h-16 border border-gray-300 border-solid border-x-0 border-t-0 bg-white z-50">
+      <div className="w-full py-2 px-5 grid grid-cols-3/auto-1fr-0.5fr items-center">
+        <div className="flex items-center gap-1.5 w-60">
+          <icon.BiGhost className="text-2xl" />
+          <h3 className="text-xl">Cloud+</h3>
         </div>
         <label htmlFor="search" className="flex items-center gap-3.5 w-full bg-gray-100 py-3 px-3.5 rounded-xl">
           <icon.BiSearch className="text-2xl" />
@@ -18,6 +18,7 @@ function Navbar() {
             className="bg-transparent w-full"
             placeholder="Search documents"
           />
+          <icon.BiSlider className="text-2xl" />
         </label>
         <div className="flex items-center justify-end gap-5">
           <icon.BiInfoCircle className="text-2xl" />
