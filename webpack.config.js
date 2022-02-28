c2fb2a4229704a960b8e6d77bdaa259149d94efd
febriadj <iamfebriadji@gmail.com>
2022-02-28 20:12:43 +0700

    :wrench: Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
           'postcss-loader',
         ],
       },
+      {
+        test: /\.(jpe?g|png|gif|webp)$/,
+        loader: 'file-loader',
+      },
     ],
   },
   devServer: {
