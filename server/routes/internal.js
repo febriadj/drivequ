82e7de5f6a387a50179ebb54027508270bcd1b4a
febriadj <iamfebriadji@gmail.com>
2022-03-01 20:14:13 +0700

    :hammer: Update server internal routes

--- server/routes/internal.js
+++ server/routes/internal.js
@@ -3,6 +3,7 @@ const document = require('../controllers/internal/document');
 const folder = require('../controllers/internal/folder');
 
 router.get('/documents', document.find);
+router.get('/documents/file/:filename', document.open);
 router.post('/documents', document.insert);
 
 router.get('/folders', folder.find);
