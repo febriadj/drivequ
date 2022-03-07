00bce2034fc9f3f2c35725484d85bb77dee44ffe
febriadj <iamfebriadji@gmail.com>
2022-03-07 20:07:53 +0700

    :hammer: Update sidebar component

--- client/components/sidebar.jsx
+++ client/components/sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as icon from 'react-icons/bi';
 
-function Sidebar() {
+function Sidebar({ page }) {
   return (
     <div className="absolute w-60 h-full bg-white">
       <div className="mt-16 pt-5">
@@ -12,17 +12,17 @@ function Sidebar() {
             <p className="text-base">New</p>
           </button>
           <div className="grid mt-5">
-            <Link to="/" className="flex items-center gap-5 bg-blue-200 py-2.5 px-5 rounded-r-3xl">
+            <Link to="/" className={`flex items-center gap-5 py-2.5 px-5 rounded-r-3xl ${page === '/' && 'bg-gray-100'}`}>
               <icon.BiHomeSmile className="text-2xl" />
               <p className="text-base">My Storage</p>
             </Link>
-            <Link to="/trash" className="flex items-center gap-5 py-2.5 px-5 rounded-r-3xl hover:bg-gray-100">
+            <Link to="/trash" className={`flex items-center gap-5 py-2.5 px-5 rounded-r-3xl ${page === '/trash' && 'bg-gray-100'}`}>
               <icon.BiTrashAlt className="text-2xl" />
               <p className="text-base">Trash</p>
             </Link>
           </div>
           <div className="border-0 border-t border-solid border-gray-300 mt-5 p-5">
-            <span className="flex items-center gap-5 rounded-r-3xl hover:bg-gray-100">
+            <span className="flex items-center gap-5 rounded-r-3xl hover:bg-gray-50">
               <icon.BiDockLeft className="text-2xl" />
               <p className="text-base">Documentation</p>
             </span>
