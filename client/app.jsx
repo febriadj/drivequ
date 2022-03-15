d35f758c9b10b487d5e92b496ad6066deac97153
febriadj <iamfebriadji@gmail.com>
2022-03-15 16:49:56 +0700

    :passport_control: Set up user authentication & access control

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import './style.css';
 
 import * as page from './pages';
 
 function App() {
+  const token = localStorage.getItem('token');
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch({
+      type: 'isLoggedIn',
+      payload: !!token,
+    });
+  }, [isLoggedIn]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<page.myStorage />} />
-        <Route exact path="/folder/:url" element={<page.docInFolder />} />
-        <Route exact path="/trash" element={<page.trash />} />
+        {
+          !isLoggedIn
+            ? <Route exact path="/" element={<page.auth />}></Route>
+            : (
+              < >
+                <Route exact path="/" element={<page.myStorage />} />
+                <Route exact path="/folder/:url" element={<page.docInFolder />} />
+                <Route exact path="/trash" element={<page.trash />} />
+              </>
+            )
+        }
         <Route path="*" element={<page.notfound />} />
       </Routes>
     </BrowserRouter>
