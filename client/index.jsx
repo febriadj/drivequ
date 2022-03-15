92e4818c6e4789b073bbb5dd01b32c01ad404dbb
febriadj <iamfebriadji@gmail.com>
2022-03-15 16:35:05 +0700

    :hammer: Add reduxjs store provider

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import { Provider } from 'react-redux';
+
 import App from './app';
+import store from './redux/store';
 
 axios.defaults.baseURL = 'http://localhost:5050/api/in';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
