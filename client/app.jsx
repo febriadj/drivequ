e35c561b3756005987bdda48014a589c7c6a6d44
febriadj <iamfebriadji@gmail.com>
2022-03-17 14:30:21 +0700

    :passport_control: Update user authentication & access control

--- client/app.jsx
+++ client/app.jsx
@@ -1,27 +1,45 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './style.css';
 
 import * as page from './pages';
+import { login, logout } from './redux/features/user';
 
 function App() {
   const token = localStorage.getItem('token');
   const dispatch = useDispatch();
-  const { isLoggedIn } = useSelector((state) => state);
+  const { user, isLoggedIn } = useSelector((state) => state.auth);
+
+  const handleLogin = async () => {
+    try {
+      const { data } = await axios.get('/user', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      dispatch(login(data.payload));
+    }
+    catch (error0) {
+      console.error(error0.response.data.message);
+    }
+  };
 
   useEffect(() => {
-    dispatch({
-      type: 'isLoggedIn',
-      payload: !!token,
-    });
+    if (token) {
+      handleLogin();
+    } else {
+      dispatch(logout());
+    }
   }, [isLoggedIn]);
 
   return (
     <BrowserRouter>
       <Routes>
         {
-          !isLoggedIn
+          !isLoggedIn && !user
             ? <Route exact path="/" element={<page.auth />}></Route>
             : (
               < >
