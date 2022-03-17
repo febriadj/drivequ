fb685369c3675501f67494865604fcdae884127c
febriadj <iamfebriadji@gmail.com>
2022-03-17 14:21:52 +0700

    :hammer: Update server internal routes

--- server/routes/internal.js
+++ server/routes/internal.js
@@ -8,18 +8,18 @@ const folder = require('../controllers/internal/folder');
 
 router.post('/register', user.register);
 router.post('/login', user.login);
-router.get('/user/:id', authenticate, user.find);
+router.get('/user', authenticate, user.find);
 
-router.post('/trash', trash.insert);
-router.get('/trash', trash.find);
-router.delete('/trash', trash.clear);
+router.post('/trash', authenticate, trash.insert);
+router.get('/trash', authenticate, trash.find);
+router.delete('/trash', authenticate, trash.clear);
 
-router.get('/documents', document.find);
+router.get('/documents', authenticate, document.find);
 router.get('/documents/file/:filename', document.open);
-router.post('/documents', document.insert);
-router.delete('/documents/trash', document.trashed);
+router.post('/documents', authenticate, document.insert);
+router.put('/documents/trashing', authenticate, document.trashed);
 
-router.get('/folders', folder.find);
-router.post('/folders', folder.insert);
+router.get('/folders', authenticate, folder.find);
+router.post('/folders', authenticate, folder.insert);
 
 module.exports = router;
