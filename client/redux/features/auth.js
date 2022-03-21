697caea9cd8a94f20a8940e01541c07aacb8c8e9
febriadj <iamfebriadji@gmail.com>
2022-03-21 15:17:35 +0700

    :hammer: Add redux/features/auth

---
+++ client/redux/features/auth.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    isLoggedIn: false,
+  },
+  reducers: {
+    login(state, action) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.user = null;
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
