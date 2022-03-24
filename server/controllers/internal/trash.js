4221c58ab889e5f549e08aef6bef318930236857
febriadj <iamfebriadji@gmail.com>
2022-03-24 17:13:02 +0700

    :hammer: Update trash controller in internal router

--- server/controllers/internal/trash.js
+++ server/controllers/internal/trash.js
@@ -55,7 +55,12 @@ exports.find = async (req, res) => {
     const docs = await DocModel.find(query).sort({ name: 1 });
     const folders = await FolderModel.find(query).sort({ name: 1 });
 
-    const merge = [...docs, ...folders].sort((a, b) => a.name > b.name);
+    const d = docs.filter((e) => !folders.map(({ url }) => url).includes(e.location));
+    const f = folders.filter((e) => (
+      !folders.map(({ url }) => url).includes(e.location[e.location.length - 1])
+    ));
+
+    const merge = [...d, ...f].sort((a, b) => a.name > b.name);
 
     response({
       res,
@@ -73,7 +78,7 @@ exports.find = async (req, res) => {
   }
 };
 
-exports.clear = async (req, res) => {
+exports.delete = async (req, res) => {
   try {
     const query = {
       $and: [
@@ -84,11 +89,20 @@ exports.clear = async (req, res) => {
     };
 
     const docs = await DocModel.find(query);
+    const folderUrl = (await FolderModel.find(query)).map(({ url }) => url);
+
+    await FolderModel.deleteMany({
+      location: {
+        $elemMatch: {
+          $in: folderUrl,
+        },
+      },
+    });
 
     await DocModel.deleteMany(query);
     await FolderModel.deleteMany(query);
 
-    const root = path.resolve(__dirname, '../../../uploads');
+    const root = path.resolve(__dirname, `../../../uploads/${req.user.id}`);
 
     let i = 0;
     while (i < docs.length) {
@@ -101,6 +115,44 @@ exports.clear = async (req, res) => {
       res,
       message: 'Trash has been emptied',
       payload: null,
+    });
+  }
+  catch (error0) {
+    response({
+      res,
+      message: error0.message,
+      success: false,
+      httpStatusCode: 400,
+    });
+  }
+};
+
+exports.recover = async (req, res) => {
+  try {
+    const obj = [
+      {
+        $or: [
+          {
+            $and: [
+              { userId: { $eq: req.user.id } },
+              { _id: { $in: req.body } },
+              { trashed: { $eq: true } },
+            ],
+          },
+          { parents: { $elemMatch: { $in: req.body } } },
+        ],
+      },
+      { $set: { trashed: false } },
+      { multi: true },
+    ];
+
+    const document = await DocModel.updateMany(obj[0], obj[1], obj[2]);
+    const folder = await FolderModel.updateMany(obj[0], obj[1], obj[2]);
+
+    response({
+      res,
+      message: '',
+      payload: { document, folder },
     });
   }
   catch (error0) {
