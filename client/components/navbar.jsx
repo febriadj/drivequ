19cc0d0a5a099b12ba4b8e9bea2d18dbca7b310b
febriadj <iamfebriadji@gmail.com>
2022-03-26 13:28:22 +0700

    :hammer: Update navbar component

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -7,16 +7,16 @@ function Navbar() {
   const [profileIsOpen, setProfileIsOpen] = useState(false);
 
   return (
-    <div className="absolute w-full h-16 border border-gray-300 border-solid border-x-0 border-t-0 bg-white z-50">
+    <div className="absolute w-full h-16 border border-gray-300 border-solid border-x-0 border-t-0 bg-white z-10">
       {
-        profileIsOpen && <Profile />
+        profileIsOpen && <Profile setProfileIsOpen={setProfileIsOpen} />
       }
       <div className="w-full py-2 px-5 grid grid-cols-3/auto-1fr-0.5fr items-center">
         <div className="flex items-center gap-1.5 w-60">
           <icon.BiCloudSnow className="text-3xl" />
           <h3 className="text-xl">CloudSync</h3>
         </div>
-        <label htmlFor="search" className="flex items-center gap-3.5 w-full bg-gray-100 py-3 px-3.5 rounded-lg">
+        <label htmlFor="search" className="flex items-center gap-3.5 w-full bg-gray-100 py-1.5 pl-3.5 pr-1.5 rounded-lg">
           <icon.BiSearch className="text-2xl" />
           <input
             type="text"
@@ -25,7 +25,12 @@ function Navbar() {
             className="bg-transparent w-full"
             placeholder="Search documents"
           />
-          <icon.BiSlider className="text-2xl" />
+          <button
+            type="button"
+            className="cursor-pointer hover:bg-gray-200 py-1.5 px-2.5 rounded-md"
+          >
+            <icon.BiSlider className="text-2xl" />
+          </button>
         </label>
         <div className="flex items-center justify-end gap-5">
           <img
