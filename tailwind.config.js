f4aa0de3fbe12a6361001b6a448fe3318e703ea2
febriadj <iamfebriadji@gmail.com>
2022-03-30 22:13:36 +0700

    :wrench: Add tailwind-scrollbar plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
+const tailwindScrollbar = require('tailwind-scrollbar');
+
 module.exports = {
   content: ['./client/**/*.{js,jsx,tsx}'],
   theme: {
     extend: {
       gridTemplateColumns: {
-        '4/1fr-0.5fr-0.5fr-0.5fr': '1fr 0.5fr 0.5fr 0.5fr',
-        '3/auto-1fr-0.5fr': 'auto 1fr 0.5fr',
         '2/1fr-auto': '1fr auto',
         '2/auto-1fr': 'auto 1fr',
+        '2/1fr-0.3fr': '1fr 0.3fr',
+        '4/1fr-0.5fr-0.5fr-0.5fr': '1fr 0.5fr 0.5fr 0.5fr',
+        '3/auto-1fr-auto': 'auto 1fr auto',
+        '3/auto-1fr-0.5fr': 'auto 1fr 0.5fr',
       },
       gridTemplateRows: {
         '2/auto-1fr': 'auto 1fr',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    tailwindScrollbar,
+  ],
 };
