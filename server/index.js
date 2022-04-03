d641f768e4eda4f085ffeb2b0d5e5dc13f16ffc5
febriadj <iamfebriadji@gmail.com>
2022-04-03 14:13:51 +0700

    :hammer: Update server index file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const server = require('./server');
+const config = require('./config');
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || config.port;
 
 server.listen(port);
 console.log(`[${port}] server running...`);
