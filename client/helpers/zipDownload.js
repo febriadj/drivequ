d3bc66821e39f4df1792cfe01577fec3caa2409a
febriadj <iamfebriadji@gmail.com>
2022-04-06 14:11:23 +0700

    :hammer: Add helpers/zipDownload

---
+++ client/helpers/zipDownload.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export default async (payload = null) => {
+  try {
+    const token = localStorage.getItem('token');
+
+    const { data } = await axios({
+      method: 'post',
+      url: '/zip',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { payload },
+    });
+
+    if (data.success) {
+      const el = document.createElement('a');
+      el.style = 'display: none';
+
+      el.href = axios.defaults.baseURL + data.payload.url;
+      el.download = data.payload.filename;
+      el.click();
+
+      el.remove();
+    }
+
+    return true;
+  }
+  catch (error0) {
+    console.error(error0.response.data.message);
+    return true;
+  }
+};
