a630dd4bc6528a4b7c13388de6d2d17c8a7eb950
febriadj <iamfebriadji@gmail.com>
2022-04-07 14:13:45 +0700

    :hammer: Update trash/table component

--- client/components/trash/table.jsx
+++ client/components/trash/table.jsx
@@ -5,7 +5,9 @@ import * as helper from '../../helpers';
 
 function Table({
   documents,
+  folders,
   setSelected,
+  size,
 }) {
   const [doubleKeys, setDoubleKeys] = useState({
     multipleSelect: false,
@@ -48,36 +50,65 @@ function Table({
     }
   };
 
+  const [init, setInit] = useState(true);
+
   useEffect(() => {
-    window.addEventListener('keydown', (event) => {
-      if (event.key === 'Control') {
+    if (init) {
+      window.addEventListener('keydown', (event) => {
+        if (event.key === 'Control') {
+          setDoubleKeys((prev) => ({
+            ...prev,
+            multipleSelect: true,
+          }));
+        }
+      });
+
+      window.addEventListener('keyup', () => {
         setDoubleKeys((prev) => ({
           ...prev,
-          multipleSelect: true,
+          multipleSelect: false,
         }));
-      }
-    });
+      });
 
-    window.addEventListener('keyup', () => {
-      setDoubleKeys((prev) => ({
-        ...prev,
-        multipleSelect: false,
-      }));
-    });
+      setInit(false);
+    }
   }, []);
 
   return (
-    <div className="absolute w-full flex mt-20">
+    <div className="absolute w-full flex mt-20 pb-5">
       <table className="w-full border-collapse md:mr-5">
         <thead>
           <tr className="h-12 px-1.5 sm:px-0 grid sm:grid-cols-2/1fr-0.3fr md:grid-cols-4/1fr-0.5fr-0.5fr-0.5fr items-center border-0 border-b border-solid border-gray-300">
-            <td>Name</td>
-            <td className="hidden md:block">Mimetype</td>
-            <td className="hidden md:block">Published</td>
-            <td className="hidden sm:block">Size</td>
+            <td className="font-semibold">Name</td>
+            <td className="hidden md:block font-semibold">Mimetype</td>
+            <td className="hidden md:block font-semibold">Updated</td>
+            <td className="hidden sm:block font-semibold">Size</td>
           </tr>
         </thead>
         <tbody>
+          {
+            folders.map((item) => (
+              <tr
+                key={item._id}
+                className="h-12 px-1.5 sm:px-0 grid sm:grid-cols-2/1fr-0.3fr md:grid-cols-4/1fr-0.5fr-0.5fr-0.5fr items-center border-0 border-b border-solid border-gray-300 cursor-default"
+                onClick={(event) => {
+                  handleOnSelection(event, {
+                    id: item._id,
+                    type: item.type,
+                    url: item.url,
+                  });
+                }}
+              >
+                <td className="flex items-center gap-3.5 overflow-x-hidden">
+                  <icon.BiFolder className="text-2xl" />
+                  <p className="text-base truncate">{item.name}</p>
+                </td>
+                <td className="hidden md:block"><span className="block w-3 h-px bg-black"></span></td>
+                <td className="hidden md:block truncate">{helper.formatDate(item.updatedAt)}</td>
+                <td className="hidden sm:block"><span className="block w-3 h-px bg-black"></span></td>
+              </tr>
+            ))
+          }
           {
             documents.map((item) => (
               <tr
@@ -91,52 +122,37 @@ function Table({
                   });
                 }}
               >
-                {
-                  item.type === 'folder' && (
-                    <td className="flex items-center gap-3.5 overflow-x-hidden">
-                      <icon.BiFolder className="text-2xl" />
-                      <p className="text-base truncate">{item.name}</p>
-                    </td>
-                  )
-                }
-                {
-                  item.type === 'file' && (
-                    <td className="grid grid-cols-2/auto-1fr items-center gap-3.5 overflow-x-hidden">
-                      {
-                        /image/.test(item.mimetype) ? (
-                          <span className="relative w-6 h-6 overflow-hidden flex justify-center items-center bg-gray-800">
-                            <img
-                              src={`${axios.defaults.baseURL}/documents/${item.userId}/file${item.url}`}
-                              alt=""
-                              className="w-[100%] h-[100%]"
-                            />
-                          </span>
-                        ) : (
-                          <icon.BiFileBlank className="text-2xl" />
-                        )
-                      }
-                      <p className="truncate">{`${item.filename}.${item.format}`}</p>
-                    </td>
-                  )
-                }
-                {
-                  item.type === 'file' ? (
-                    <td className="truncate hidden md:block">{item.mimetype}</td>
-                  ) : (
-                    <td><span className="w-3 h-px bg-black hidden md:block"></span></td>
-                  )
-                }
-                <td className="truncate hidden md:block">{helper.formatDate(item.createdAt)}</td>
-                {
-                  item.type === 'file' ? (
-                    <td className="truncate hidden sm:block">{helper.bytesToSize(item.size)}</td>
-                  ) : (
-                    <td><span className="w-3 h-px bg-black hidden sm:block"></span></td>
-                  )
-                }
+                <td className="grid grid-cols-2/auto-1fr items-center gap-3.5 overflow-x-hidden">
+                  {
+                    /image/.test(item.mimetype) ? (
+                      <span className="relative w-6 h-6 overflow-hidden flex justify-center items-center bg-gray-800">
+                        <img
+                          src={`${axios.defaults.baseURL}/documents/${item.userId}/file${item.url}`}
+                          alt=""
+                          className="w-[100%] h-[100%]"
+                        />
+                      </span>
+                    ) : (
+                      <icon.BiFileBlank className="text-2xl" />
+                    )
+                  }
+                  <p className="truncate">{`${item.filename}.${item.format}`}</p>
+                </td>
+                <td className="hidden md:block truncate">{item.mimetype}</td>
+                <td className="hidden md:block truncate">{helper.formatDate(item.updatedAt)}</td>
+                <td className="hidden sm:block truncate">{helper.bytesToSize(item.size)}</td>
               </tr>
             ))
           }
+          <tr className="flex items-center gap-1.5 mt-2.5 px-2.5 sm:px-1.5">
+            <td className="bg-gray-100 px-2.5 rounded-md cursor-default hover:bg-gray-200">{`Folder: ${folders.length}`}</td>
+            <td className="bg-gray-100 px-2.5 rounded-md cursor-default hover:bg-gray-200">{`File: ${documents.length}`}</td>
+            <td
+              className="font-semibold"
+            >
+              {helper.bytesToSize(size)}
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
