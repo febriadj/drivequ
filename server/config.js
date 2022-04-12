2ab00dcbdb3f28e68909286f2bc08866a61c3f35
febriadj <iamfebriadji@gmail.com>
2022-04-12 14:34:10 +0700

    :wrench: Update server config file

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   port: 5050,
   database: {
-    uri: process.env.MONGO_URI || 'mongodb://localhost:27017/cloudsync',
+    uri: isDev ? 'mongodb://localhost:27017/cloudsync' : process.env.MONGO_URI,
   },
 };
