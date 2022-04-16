a290e7466378ab69ef83beaf4d9d5ea35b2e235f
febriadj <iamfebriadji@gmail.com>
2022-04-17 00:37:42 +0700

    :hammer: Update helpers/randomStr

--- server/helpers/randomStr.js
+++ server/helpers/randomStr.js
@@ -1,10 +1,10 @@
-module.exports = () => {
+module.exports = (length = 16) => {
   const init = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
   let i = 0;
   let str = '';
 
-  while (i < 16) {
+  while (i < length) {
     str += init.charAt(Math.floor(Math.random() * init.length));
     i += 1;
   }
