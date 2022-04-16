6b7c83deef8d2a1dd31a84039cbefe14a97095ae
febriadj <iamfebriadji@gmail.com>
2022-04-16 13:40:16 +0700

    :hammer: Update searchTab component

--- client/components/searchTab.jsx
+++ client/components/searchTab.jsx
@@ -4,8 +4,9 @@ import * as icon from 'react-icons/bi';
 import * as helper from '../helpers';
 
 function SearchTab({
-  form,
   setForm,
+  form,
+  setSearchTabIsOpen,
 }) {
   const token = localStorage.getItem('token');
   const [documents, setDocuments] = useState([]);
@@ -37,25 +38,51 @@ function SearchTab({
   }, [form.filename]);
 
   return (
-    <div className="bg-white z-10 pt-2.5 pb-3.5 max-h-80 grid grid-rows-2/1fr-auto">
-      <div className="relative overflow-y-scroll mr-3.5">
+    <div className="bg-white z-10 pt-2.5 pb-3.5 max-h-80 grid grid-rows-2/1fr-auto" id="searchTab">
+      <div className="relative overflow-y-auto mr-3.5">
         <table className="w-full">
           <tbody>
             {
               documents.map((item) => (
                 <tr
                   key={item._id}
-                  className="border-0 border-b border-solid border-gray-300 h-12 grid grid-cols-2/1fr-0.3fr items-center"
+                  className="border-0 border-b border-solid border-gray-300 h-16 grid sm:grid-cols-2/1fr-0.3fr items-center cursor-default"
+                  onClick={(event) => {
+                    const tags = [...document.querySelectorAll('#searchTab tr')];
+                    tags.map((tr) => tr.classList.remove('bg-gray-100'));
+
+                    event.currentTarget.classList.add('bg-gray-100');
+                  }}
+                  onDoubleClick={() => {
+                    const link = document.createElement('a');
+                    link.href = `${axios.defaults.baseURL}/documents/${item.userId}/file${item.url}`;
+
+                    link.click();
+                    link.remove();
+
+                    setSearchTabIsOpen(false);
+                  }}
                 >
-                  <td className="truncate flex items-center gap-2.5 py-3">
-                    <img
-                      src={`${axios.defaults.baseURL}/documents/${item.userId}/file${item.url}`}
-                      alt=""
-                      className="w-6 h-6"
-                    />
-                    <p>{`${item.filename}.${item.format}`}</p>
+                  <td className="truncate flex items-center gap-3.5">
+                    {
+                      /image/.test(item.mimetype) ? (
+                        <span className="relative w-6 h-6 overflow-hidden flex justify-center items-center bg-gray-800">
+                          <img
+                            src={`${axios.defaults.baseURL}/documents/${item.userId}/file${item.url}`}
+                            alt=""
+                            className="w-[100%] h-[100%]"
+                          />
+                        </span>
+                      ) : (
+                        <icon.BiFileBlank className="text-2xl" />
+                      )
+                    }
+                    <div className="grid overflow-x-hidden">
+                      <p className="font-semibold truncate">{`${item.filename}.${item.format}`}</p>
+                      <p className="text-sm truncate">{item.originalname}</p>
+                    </div>
                   </td>
-                  <td className="truncate text-right">{helper.formatDate(item.createdAt)}</td>
+                  <td className="hidden sm:block truncate text-right">{helper.formatDate(item.createdAt)}</td>
                 </tr>
               ))
             }
