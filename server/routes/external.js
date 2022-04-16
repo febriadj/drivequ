01dc2f648a5b0e164e1f405142a9bdbf64837076
febriadj <iamfebriadji@gmail.com>
2022-04-17 00:36:25 +0700

    :hammer: Update server external routes

--- server/routes/external.js
+++ server/routes/external.js
@@ -1,21 +1,16 @@
 const router = require('express').Router();
 const authenticate = require('../middleware/external/auth');
 
-// const trash = require('../controllers/internal/trash');
 const document = require('../controllers/external/document');
 const folder = require('../controllers/external/folder');
 
-// router.post('/trash', trash.insert);
-// router.get('/trash', trash.find);
-// router.delete('/trash', trash.clear);
-
 router.get('/documents', authenticate, document.find);
-router.get('/documents/file/:filename', document.open);
+router.get('/documents/:id/file/:filename', document.open);
 router.post('/documents', authenticate, document.insert);
-router.put('/documents/trashing', authenticate, document.trashing);
 router.delete('/documents', authenticate, document.delete);
 
 router.get('/folders', authenticate, folder.find);
 router.post('/folders', authenticate, folder.insert);
+router.delete('/folders', authenticate, folder.delete);
 
 module.exports = router;
