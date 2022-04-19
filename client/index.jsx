042025f0b209324db466792bafcc995dbc4dc34a
febriadj <iamfebriadji@gmail.com>
2022-04-19 17:40:07 +0700

    :alien: Update axios base URL

--- client/index.jsx
+++ client/index.jsx
@@ -7,7 +7,7 @@ import App from './app';
 import store from './redux/store';
 
 const isDev = process.env.NODE_ENV === 'development';
-axios.defaults.baseURL = isDev ? 'http://localhost:5050/api/in' : '/api/in';
+axios.defaults.baseURL = isDev ? 'http://localhost:5050' : '';
 
 ReactDOM.render(
   <Provider store={store}>
