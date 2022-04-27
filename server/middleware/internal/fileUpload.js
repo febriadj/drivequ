c2ef9b985a10f9915c1fdf173252a792c06c2f65
febriadj <iamfebriadji@gmail.com>
2022-04-27 16:23:31 +0700

    :hammer: Update internal/fileUpload middleware

--- server/middleware/internal/fileUpload.js
+++ server/middleware/internal/fileUpload.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const fs = require('fs');
 const multer = require('multer');
 
 const randomStr = require('../../helpers/randomStr');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, path.join(__dirname, '../../../uploads'));
+    const base = path.join(__dirname, '../../../uploads');
+
+    if (!fs.existsSync(base)) fs.mkdirSync(base);
+    cb(null, base);
   },
   filename(req, file, cb) {
     cb(null, randomStr());
