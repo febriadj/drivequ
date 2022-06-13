e82cc55fb778f196ee7e556d1b26c32a1734d2e5
febriadj <iamfebriadji@gmail.com>
2022-06-14 01:15:31 +0700

    :hammer: Update server config file

--- server/config.js
+++ server/config.js
@@ -1,8 +1,21 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
+  isDev,
   port: 5050,
   database: {
-    uri: isDev ? 'mongodb://localhost:27017/cloudsync' : process.env.MONGO_URI,
+    uri: {
+      prod: process.env.MONGO_URI,
+      dev: 'mongodb://localhost:27017/drivequ',
+    },
+  },
+  fileUpload: {
+    max: 2 * 1000 * 1000, // 2 mb
+    types: [
+      'application',
+      'text',
+      'image',
+      'audio',
+    ],
   },
 };
