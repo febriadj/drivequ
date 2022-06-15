9173b8805f579f1a3f17856989cc1c15077bdec0
febriadj <iamfebriadji@gmail.com>
2022-06-16 01:19:58 +0700

    :hammer: Update auth/regiter component

--- client/components/auth/register.jsx
+++ client/components/auth/register.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import * as icon from 'react-icons/bi';
-import baseConfig from '../../config';
 
 function Register({ setLoginFormIsOpen }) {
   const [form, setForm] = useState({
@@ -71,45 +70,47 @@ function Register({ setLoginFormIsOpen }) {
     }
   };
 
-  const fieldStyle = 'py-2.5 px-3.5 border border-solid border-gray-300 rounded-lg grid grid-cols-2/auto-1fr items-center gap-3.5 hover:border-black';
-
   return (
-    <div className="absolute w-full h-full overflow-y-scroll p-5">
+    <div className="absolute w-full h-full overflow-y-scroll py-10 px-5 md:p-10">
       <div>
-        <span className="flex items-center gap-2.5 mb-2.5">
-          <h1 className="text-base opacity-50">Sign In</h1>
-          <span className="block w-1 h-1 bg-black rounded-[50%]"></span>
-          <h1 className="text-3xl font-semibold">Sign Up</h1>
-        </span>
-        <p>Complete the form below to register your account and start subscribing to our service</p>
+        <h1 className="text-2xl font-bold mb-2">Sign Up</h1>
+        <p>Complete all forms to register your account on the DriveQu service.</p>
       </div>
       <form method="post" className="w-full grid gap-10 my-10" onSubmit={handleSubmit}>
-        <span className="grid grid-cols-2/1fr-auto gap-2.5">
-          <label htmlFor="app" className={fieldStyle}>
-            <icon.BiRocket className="text-xl" />
+        <span className="grid grid-cols-2/1fr-auto gap-2">
+          <label htmlFor="app" className="relative flex items-center">
+            <icon.BiRocket className="text-xl absolute left-0 translate-x-3" />
             <input
               type="text"
               name="app"
               id="app"
-              className="bg-transparent w-full"
-              placeholder="Application Name"
+              className="peer bg-transparent w-full py-3 px-11 rounded-md border border-solid border-gray-300 focus:border-black valid:bg-gray-50"
+              placeholder="Application name"
               value={form.app}
               onChange={handleChange}
               required
             />
+            {
+              form.app.length > 0 && (
+                <>
+                  <icon.BiError className="text-xl absolute right-0 -translate-x-3 hidden text-red-600 peer-invalid:block" />
+                  <icon.BiCheckCircle className="text-xl absolute right-0 -translate-x-3 hidden text-emerald-600 peer-valid:block" />
+                </>
+              )
+            }
           </label>
           <div className="relative flex w-32">
             <button
               type="button"
               onClick={() => setModalOption((prev) => !prev)}
-              className="w-full flex justify-between items-center gap-1.5 py-2.5 pl-3.5 pr-2.5 bg-gray-50 border border-solid border-gray-300 rounded-lg hover:bg-gray-100"
+              className="w-full flex justify-between items-center gap-1 p-2 pl-3 bg-gray-50 border border-solid border-gray-300 rounded-lg hover:bg-gray-100"
             >
               <p className="first-letter:uppercase">{form.accType}</p>
               <icon.BiChevronDown className="text-2xl" />
             </button>
             {
               modalOption && (
-                <div className="absolute w-full top-0 bg-white z-10 py-2.5 translate-y-12 shadow-lg shadow-gray-200">
+                <div className="absolute w-full top-0 bg-white z-10 py-2 translate-y-12 shadow-lg shadow-gray-200">
                   <button
                     type="button"
                     onClick={() => {
@@ -118,7 +119,7 @@ function Register({ setLoginFormIsOpen }) {
                         setModalOption(false);
                       }, 200);
                     }}
-                    className="p-2.5 hover:bg-gray-100 w-full flex items-center gap-2.5"
+                    className="p-2 hover:bg-gray-100 w-full flex items-center gap-2.5"
                   >
                     { form.accType === 'personal' ? <icon.BiRadioCircleMarked /> : <icon.BiRadioCircle /> }
                     <p>Personal</p>
@@ -131,7 +132,7 @@ function Register({ setLoginFormIsOpen }) {
                         setModalOption(false);
                       }, 200);
                     }}
-                    className="p-2.5 hover:bg-gray-100 w-full flex items-center gap-2.5"
+                    className="p-2 hover:bg-gray-100 w-full flex items-center gap-2.5"
                   >
                     { form.accType === 'business' ? <icon.BiRadioCircleMarked /> : <icon.BiRadioCircle /> }
                     <p>Business</p>
@@ -141,120 +142,171 @@ function Register({ setLoginFormIsOpen }) {
             }
           </div>
         </span>
-        <span className="grid gap-2.5">
+        <span className="grid gap-2">
           {
             form.accType === 'personal' ? (
-              <div className="flex gap-2.5">
-                <label htmlFor="firstname" className={fieldStyle}>
-                  <icon.BiUser className="text-xl" />
+              <div className="flex gap-2">
+                <label htmlFor="firstname" className="relative flex items-center">
+                  <icon.BiUser className="text-xl absolute left-0 translate-x-3" />
                   <input
                     type="text"
                     name="firstname"
                     id="firstname"
-                    className="bg-transparent w-full"
-                    placeholder="First Name"
+                    className="peer bg-transparent w-full py-3 px-11 rounded-md border border-solid border-gray-300 focus:border-black valid:bg-gray-50"
+                    placeholder="First name"
                     value={form.firstname}
                     onChange={handleChange}
                     required
                   />
+                  {
+                    form.firstname.length > 0 && (
+                      <>
+                        <icon.BiError className="text-xl absolute right-0 -translate-x-3 hidden text-red-600 peer-invalid:block" />
+                        <icon.BiCheckCircle className="text-xl absolute right-0 -translate-x-3 hidden text-emerald-600 peer-valid:block" />
+                      </>
+                    )
+                  }
                 </label>
-                <label htmlFor="lastname" className={fieldStyle}>
-                  <icon.BiUser className="text-xl" />
+                <label htmlFor="lastname" className="relative flex items-center">
+                  <icon.BiUser className="text-xl absolute left-0 translate-x-3" />
                   <input
                     type="text"
                     name="lastname"
                     id="lastname"
-                    className="bg-transparent w-full"
-                    placeholder="Last Name"
+                    className="peer bg-transparent w-full py-3 px-11 rounded-md border border-solid border-gray-300 focus:border-black valid:bg-gray-50"
+                    placeholder="First name"
                     value={form.lastname}
                     onChange={handleChange}
                     required
                   />
+                  {
+                    form.lastname.length > 0 && (
+                      <>
+                        <icon.BiError className="text-xl absolute right-0 -translate-x-3 hidden text-red-600 peer-invalid:block" />
+                        <icon.BiCheckCircle className="text-xl absolute right-0 -translate-x-3 hidden text-emerald-600 peer-valid:block" />
+                      </>
+                    )
+                  }
                 </label>
               </div>
             ) : (
-              <label htmlFor="company" className={fieldStyle}>
-                <icon.BiBuildingHouse className="text-xl" />
+              <label htmlFor="company" className="relative flex items-center">
+                <icon.BiBuildingHouse className="text-xl absolute left-0 translate-x-3" />
                 <input
                   type="text"
                   name="company"
                   id="company"
-                  className="bg-transparent w-full"
-                  placeholder="Company Name"
+                  className="peer bg-transparent w-full py-3 px-11 rounded-md border border-solid border-gray-300 focus:border-black valid:bg-gray-50"
+                  placeholder="Company"
                   value={form.company}
                   onChange={handleChange}
                   required
                 />
+                {
+                  form.company.length > 0 && (
+                    <>
+                      <icon.BiError className="text-xl absolute right-0 -translate-x-3 hidden text-red-600 peer-invalid:block" />
+                      <icon.BiCheckCircle className="text-xl absolute right-0 -translate-x-3 hidden text-emerald-600 peer-valid:block" />
+                    </>
+                  )
+                }
               </label>
             )
           }
-          <label htmlFor="email" className={fieldStyle}>
-            <icon.BiEnvelope className="text-xl" />
+          <label htmlFor="email" className="relative flex items-center">
+            <icon.BiEnvelope className="text-xl absolute left-0 translate-x-3" />
             <input
               type="email"
               name="email"
               id="email"
-              className="bg-transparent w-full"
-              placeholder="Email"
+              className="peer bg-transparent w-full py-3 px-11 rounded-md border border-solid border-gray-300 focus:border-black valid:bg-gray-50"
+              placeholder="Email address"
               value={form.email}
               onChange={handleChange}
               required
             />
+            {
+              form.email.length > 0 && (
+                <>
+                  <icon.BiError className="text-xl absolute right-0 -translate-x-3 hidden text-red-600 peer-invalid:block" />
+                  <icon.BiCheckCircle className="text-xl absolute right-0 -translate-x-3 hidden text-emerald-600 peer-valid:block" />
+                </>
+              )
+            }
           </label>
         </span>
-        <span className="grid gap-2.5">
-          <label htmlFor="password" className={fieldStyle}>
-            <icon.BiLockAlt className="text-xl" />
+        <span className="grid gap-2">
+          <label htmlFor="password" className="relative flex items-center">
+            <icon.BiLockOpenAlt className="text-xl absolute left-0 translate-x-3" />
             <input
               type="password"
               name="password"
               id="password"
-              className="bg-transparent w-full"
+              className="peer bg-transparent w-full py-3 px-11 rounded-md border border-solid border-gray-300 focus:border-black valid:bg-gray-50"
               placeholder="Password"
               value={form.password}
               onChange={handleChange}
               required
             />
+            {
+              form.password.length > 0 && (
+                <>
+                  <icon.BiError className="text-xl absolute right-0 -translate-x-3 hidden text-red-600 peer-invalid:block" />
+                  <icon.BiCheckCircle className="text-xl absolute right-0 -translate-x-3 hidden text-emerald-600 peer-valid:block" />
+                </>
+              )
+            }
           </label>
-          <label htmlFor="confirmPassword" className={fieldStyle}>
-            <icon.BiLockOpenAlt className="text-xl" />
+          <label htmlFor="confirmPassword" className="relative flex items-center">
+            <icon.BiLockAlt className="text-xl absolute left-0 translate-x-3" />
             <input
-              type="password"
+              type="text"
               name="confirmPassword"
               id="confirmPassword"
-              className="bg-transparent w-full"
-              placeholder="Confirm Password"
+              className="peer bg-transparent w-full py-3 px-11 rounded-md border border-solid border-gray-300 focus:border-black valid:bg-gray-50"
+              placeholder="Confirm password"
               value={form.confirmPassword}
               onChange={handleChange}
               required
             />
+            {
+              form.confirmPassword.length > 0 && (
+                <>
+                  <icon.BiError className="text-xl absolute right-0 -translate-x-3 hidden text-red-600 peer-invalid:block" />
+                  <icon.BiCheckCircle className="text-xl absolute right-0 -translate-x-3 hidden text-emerald-600 peer-valid:block" />
+                </>
+              )
+            }
           </label>
         </span>
         <div className="grid">
-          <p className={`text-base ${response.success ? 'text-black' : 'text-red-900'} first-letter:uppercase`}>{response.message}</p>
-          <div className="grid grid-cols-2/1fr-auto gap-2.5 mt-3.5">
-            <button
-              type="submit"
-              className="p-3.5 bg-blue-500 rounded-lg text-white hover:bg-blue-600"
-            >
-              Sign Up
-            </button>
-            <button
-              type="button"
-              className="p-3.5 bg-gray-100 rounded-lg w-24 hover:bg-gray-200"
-              onClick={() => {
-                setLoginFormIsOpen(true);
-              }}
-            >
-              Sign In
-            </button>
-          </div>
+          {
+            response.message && (
+              <p className={`${response.success ? 'text-black' : 'text-red-900'} mb-2 underline decoration-1 decoration-red-900 first-letter:uppercase`}>
+                {response.message}
+              </p>
+            )
+          }
+          <button
+            type="submit"
+            className="p-3 font-bold bg-blue-500 rounded-md text-white hover:bg-blue-600 border border-solid border-blue-800"
+          >
+            Sign Up
+          </button>
         </div>
       </form>
-      <div className="flex items-center gap-1">
-        <icon.BiCopyright className="text-base" />
-        <p>{`${new Date().getFullYear()} ${baseConfig.appName}`}</p>
-      </div>
+      <p>
+        <span className="mr-2">Have an account?</span>
+        <span
+          aria-hidden
+          className="cursor-pointer hover:underline decoration-1"
+          onClick={() => {
+            setLoginFormIsOpen(true);
+          }}
+        >
+          Sign In
+        </span>
+      </p>
     </div>
   );
 }
