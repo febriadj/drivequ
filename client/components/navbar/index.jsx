345af11a38ac6b6b9d398ba019c7de989a0f4583
febriadj <iamfebriadji@gmail.com>
2022-06-19 20:05:25 +0700

    :hammer: Update navbar/index component

--- client/components/navbar/index.jsx
+++ client/components/navbar/index.jsx
@@ -116,7 +116,7 @@ function Navbar() {
           <img
             src={avatar}
             alt={avatar}
-            className="w-12 h-12 rounded-[50%] cursor-pointer"
+            className="w-10 h-10 rounded-[50%] cursor-pointer"
             onClick={() => {
               setTab((prev) => ({
                 ...prev,
